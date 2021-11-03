a0286d4648af5d28c2ba302585a63420ceda1cd8
Michael <mjpj@live.com>
2021-11-02 21:44:04 -0400

    completed

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+
+
+function receivesAFunction(value) {
+     value();
+}
+
+
+function returnsANamedFunction() {
+    return function namedFunc() {
+    console.log("returns my named function");
+}
+}
+
+
+function returnsAnAnonymousFunction() {
+return () => console.log("returns anonymous function");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
